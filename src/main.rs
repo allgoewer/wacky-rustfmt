dbbdbc3aeceaeb47777071ea5894d2d90c0f5228
Maik Allgöwer <maik@llgoewer.de>
2023-03-02 18:48:01 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+#![allow(clippy::never_loop)]
+#![allow(clippy::unit_arg)]
+
+trait Wacky {
+    const CHAR: char;
+
+    fn wack(&self) {
+        println!("{}", Self::CHAR);
+    }
+}
+
+impl Wacky for () {
+    const CHAR: char = 'a';
+}
+
+impl Wacky for bool {
+    const CHAR: char = 'b';
+}
+
+fn main() {
+    {
+        // This block evaluates to () .
+        // Run rustfmt to break the formatting of the loop.
+        // This block will then evaluate to a bool.
+        loop {
+            break false
+        };
+    }
+    .wack();
+}
